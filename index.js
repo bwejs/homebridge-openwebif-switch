dd263b4c3ee2f1d9ca7eb4e7ce914b1317a6a53a
bwejs <bwejs@sedb.de>
2018-04-23 20:57:05 +0200

    Now Playing.

--- index.js
+++ index.js
@@ -8,39 +8,50 @@ var inherits = require('util').inherits;
 module.exports = function (homebridge) {
 	Service = homebridge.hap.Service;
 	Characteristic = homebridge.hap.Characteristic;
+	inherits(OpenWebifSwitchAccessory.NowPlayingService, Service);
+
 	homebridge.registerAccessory("homebridge-openwebif-switch", "OpenWebifSwitch", OpenWebifSwitchAccessory);
 }
 
 function OpenWebifSwitchAccessory(log, config) {
 	this.log = log;
-
+	this.config = config
 	this.name = config["name"];
 
 	//required
 	this.host = config["host"];
 	this.port = config["port"] || 80;
 	this.checkIntervalSeconds = config["checkIntervalSeconds"] || 120;
+	this.excludeSpeakerService = config["excludeSpeakerService"] || false;
 
 	var me = this;
 	if (this.checkIntervalSeconds > 0) {
 		setInterval(function () {
 			me.getPowerState(function (error, value) {
 				if (error) {
-						
+
 				} else {
 					me.switchService.setCharacteristic(Characteristic.On, value);
 				}
-			}); 
+			});
 			me.getVolume(function (error, value) {
 				if (error) {
-				
+
 				} else {
 					me.speakerService.setCharacteristic(Characteristic.Volume, value);
 				}
 			})
-			}, me.checkIntervalSeconds * 1000);
+		}, me.checkIntervalSeconds * 1000);
 	}
 }
+
+
+OpenWebifSwitchAccessory.NowPlayingService = function(name) {
+  Service.call(this, name, 'F7138C87-EABF-420A-BFF0-76FC04DD81CD', null);
+
+  // Optional Characteristics
+  this.UUID = 'F7138C87-EABF-420A-BFF0-76FC04DD81CD';
+};
 
 OpenWebifSwitchAccessory.prototype = {
 
@@ -97,14 +108,14 @@ OpenWebifSwitchAccessory.prototype = {
 				me.log('getVolume() failed: %s', error.message);
 				callback(error)
 			} else {
-										callback(null, targetValue);
+				callback(null, targetValue);
 
 			}
 		});
 	},
 	setMute: function (value, callback) {
 		var me = this;
-		var	tagetValue = value ? true : false; //number to boolean		
+		var	tagetValue = value ? true : false; //number to boolean
 		me.getMute(function (error, muteCurrent) {
 			if(error){
 				callback(null, targetValue ? false : true); //totally off ->
@@ -153,8 +164,8 @@ OpenWebifSwitchAccessory.prototype = {
 			if (error){
 				callback(error)
 			} else {
-				var xmlValue = data.e2volume.e2current[0];	
-				var xmlMuteValue = data.e2volume.e2ismuted[0];				
+				var xmlValue = data.e2volume.e2current[0];
+				var xmlMuteValue = data.e2volume.e2ismuted[0];
 				var percentage = parseFloat(xmlValue);
 				var boolMute = xmlMuteValue == "True";
 				var characteristicValue = percentage;// (percentage / 5.0) - 10.0; //auf 20 runterrechnen und auf -10 bis 10 ummappen.
@@ -163,7 +174,7 @@ OpenWebifSwitchAccessory.prototype = {
 			}
 		});
 	},
-	
+
 	getDiscSpace: function (callback) {
 		var me = this;
 		this._httpXMLGetForMethod("/web/about", function (error,data) {
@@ -181,6 +192,29 @@ OpenWebifSwitchAccessory.prototype = {
 			}
 		});
 	},
+	getCurrentService: function (callback) {
+		var me = this;
+		this._httpXMLGetForMethod("/web/getcurrent", function (error,data) {
+			if (error){
+				callback(error)
+			} else {
+				var title = data.e2currentserviceinformation.e2eventlist[0].e2event[0].e2eventservicename;
+				callback(null, String(title));
+			}
+		});
+	},
+	
+	getCurrentTitle: function (callback) {
+		var me = this;
+		this._httpXMLGetForMethod("/web/getcurrent", function (error,data) {
+			if (error){
+				callback(error)
+			} else {
+				var title = data.e2currentserviceinformation.e2eventlist[0].e2event[0].e2eventtitle;
+				callback(null, String(title));
+			}
+		});
+	},
 
 	identify: function (callback) {
 		this.log("Identify requested!");
@@ -190,42 +224,63 @@ OpenWebifSwitchAccessory.prototype = {
 	getServices: function () {
 		var informationService = new Service.AccessoryInformation();
 		informationService
-			.setCharacteristic(Characteristic.Manufacturer, "alex224")
-			.setCharacteristic(Characteristic.Model, "OpenWebifSwitch")
-			.setCharacteristic(Characteristic.SerialNumber, "OpenWebifSwitch Serial Number");
+		.setCharacteristic(Characteristic.Manufacturer, "alex224")
+		.setCharacteristic(Characteristic.Model, "OpenWebifSwitch")
+		.setCharacteristic(Characteristic.SerialNumber, "OpenWebifSwitch Serial Number");
 
 		this.switchService = new Service.Switch(this.name);
 		this.switchService
-			.getCharacteristic(Characteristic.On)
-			.on('get', this.getPowerState.bind(this))
-			.on('set', this.setPowerState.bind(this));
-
+		.getCharacteristic(Characteristic.On)
+		.on('get', this.getPowerState.bind(this))
+		.on('set', this.setPowerState.bind(this));
 
 		this.switchService
-			.addCharacteristic(this.makeDiscSpaceCharacteristic())
-			.on('get', this.getDiscSpace.bind(this))
+		.addCharacteristic(this.makeDiscSpaceCharacteristic())
+		.on('get', this.getDiscSpace.bind(this))
+		
+		
+		
 
-		this.speakerService = new Service.Speaker(this.name);
-		this.speakerService
+		if (this.config["includeIP"] || false) {
+			this.switchService.setCharacteristic(this.makeIPCharacteristic(this.host), this.host);
+		}
+		var services = [informationService, this.switchService];
+		
+		if (true){
+			this.nowPlayingService = new OpenWebifSwitchAccessory.NowPlayingService(this.name);
+						
+			this.nowPlayingService.addCharacteristic(this.makeNowPlayingTitleeCharacteristic())
+			.on('get', this.getCurrentTitle.bind(this));
+			
+			this.nowPlayingService.addCharacteristic(this.makeNowPlayingAlbumCharacteristic())
+			.on('get', this.getCurrentService.bind(this));
+
+			services.push(this.nowPlayingService);
+		}
+		
+		
+		if (!this.excludeSpeakerService){
+			this.speakerService = new Service.Speaker(this.name);
+			this.speakerService
 			.getCharacteristic(Characteristic.Volume)
 			.on('get', this.getVolume.bind(this))
 			.on('set', this.setVolume.bind(this));
-
-	this.speakerService
+			this.speakerService
 			.getCharacteristic(Characteristic.Mute)
 			.on('get', this.getMute.bind(this))
 			.on('set', this.setMute.bind(this));
-
-		return [informationService, this.switchService, this.speakerService ];
+			services.push(this.speakerService);
+		}
+		return services;
 	},
 
 	/**
-	 * Custom characteristic for DiscSpace
-	 *
-	 * @return {Characteristic} The DiscSpace characteristic
-	 */
+	* Custom characteristic for DiscSpace
+	*
+	* @return {Characteristic} The DiscSpace characteristic
+	*/
 	makeDiscSpaceCharacteristic : function() {
-	
+
 		var volumeCharacteristic = function() {
 			Characteristic.call(this, 'DiscSpace', 'B795302F-FFBA-41D9-9076-337986B81D27');
 			this.setProps({
@@ -236,16 +291,53 @@ OpenWebifSwitchAccessory.prototype = {
 				minStep: 1,
 				perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
 			});
-			//this.value = this.getDefaultValue();
-			this.value = 50;
+			this.value = 0;
 		};
-	
+
 		inherits(volumeCharacteristic, Characteristic);
 		return volumeCharacteristic;
 	},
 
 
-	
+	makeNowPlayingTitleeCharacteristic : function() {
+		var generated = function() {
+			Characteristic.call(this, 'Title', '00003001-0000-1000-8000-135D67EC4377');
+			this.setProps({
+				format: Characteristic.Formats.STRING,
+				perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+			});
+			this.value = "";
+		};
+		inherits(generated, Characteristic);
+		return generated;
+	},
+	makeNowPlayingAlbumCharacteristic : function() {
+		var generated = function() {
+			Characteristic.call(this, 'Sender', '00003002-0000-1000-8000-135D67EC4377');
+			this.setProps({
+				format: Characteristic.Formats.STRING,
+				perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+			});
+			this.value = "";
+		};
+
+		inherits(generated, Characteristic);
+		return generated;
+	},
+
+
+	makeIPCharacteristic : function(ip) {
+		var volumeCharacteristic = function() {
+			Characteristic.call(this, 'IP', 'B795302F-FFBA-41D9-9076-337986B81D29');
+			this.setProps({
+				format: Characteristic.Formats.STRING,
+				perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+			});
+			this.value = ip;
+		};
+		inherits(volumeCharacteristic, Characteristic);
+		return volumeCharacteristic;
+	},
 
 	_httpRequest: function (url, body, method, callback) {
 		request({
@@ -254,10 +346,11 @@ OpenWebifSwitchAccessory.prototype = {
 			method: method,
 			rejectUnauthorized: false
 		},
-			function (error, response, body) {
-				callback(error, response, body);
-			});
+		function (error, response, body) {
+			callback(error, response, body);
+		});
 	},
+
 	_httpXMLGetForMethod: function (method, callback) {
 		if (!this.host) {
 			callback(new Error("No host defined."));
